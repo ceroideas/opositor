54a94814db498fe0788bec4008c39abe50950b2c
Jorge Solano <jorgesolano92@gmail.com>
2022-07-08 09:17:18 -0400

    base

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    //
+    return redirect('login');
+});
+
+Route::get('login', ['uses' => function () {
+    return view('login');
+}, 'as' => 'login']);
+
+Route::group(['prefix' => 'admin'/*, 'middleware' => 'auth'*/], function() {
+    Route::get('/', [MainController::class, 'index']);
+    //
 });
